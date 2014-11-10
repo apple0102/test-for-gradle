50d9bd74e5e8ea1500a19a362347557abb5bd80d
kuangzai <kaungzai@gmail.com>
2014-11-10 10:10:43 +0800

    This program can add 2 nubmers. (Version 2)

--- src/p1/Exe.java
+++ src/p1/Exe.java
@@ -1,9 +1,20 @@
 package p1;
 
+import java.util.Scanner;
+
 public class Exe {
-
 	public static void main(String[] args) {
-		System.out.println("Test!!!");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("This is version 2");
+		
+		System.out.print("Input the first number: ");
+		int var1 = input.nextInt();
+		
+		System.out.print("Input the second number: ");
+		int var2 = input.nextInt();
+		
+		System.out.println("The answer of adding 2 numbers is: " + (var1+var2));
 	}
-
 }
